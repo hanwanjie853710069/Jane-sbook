2c1aaf3094bddd62ce8dcec630415445c82e082c
hanwanjie <471941655@qq.com>
2017-11-08 17:55:07 +0800

    提交代码

---
+++ temp.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+git add --all
+echo "请输入提交描述文字如果没有默认: 提交代码"
+read describe
+if [ ! -n "$describe" ]
+then
+describe="提交代码"
+fi
+echo $describe
+git commit -m $describe
+
+echo "请输入要拉取的分支名称"
+echo "0 -> master"
+echo "1 -> develop"
+echo "如果是其他分支请直接输入分支名称"
+read barch
+tempbarch=""
+if [ $barch == 0 ]
+then
+echo "输入的是master"
+tempbarch="master"
+elif [ $barch == 1 ]
+then
+echo "输入的是develop"
+tempbarch="develop"
+else
+echo "输入的是其他分支"
+tempbarch=$barch
+fi
+echo $tempbarch
+git pull origin $barch
+
+echo "请输入要上传的分支名称"
+echo "0 -> master"
+echo "1 -> develop"
+echo "如果是其他分支请直接输入分支名称"
+read barch
+pushbarch=""
+if [ $barch == 0 ]
+then
+echo "输入的是master"
+pushbarch="master"
+elif [ $barch == 1 ]
+then
+echo "输入的是develop"
+pushbarch="develop"
+else
+echo "输入的是其他分支"
+pushbarch=$barch
+fi
+echo $pushbarch
+git push origin $pushbarch
+
+echo "---END---"
+
+
+
+
+
